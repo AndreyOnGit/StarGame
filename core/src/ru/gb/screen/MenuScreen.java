4e483cd324e3b87094c6759e850e5130f61e3aa7
AndreyOnGit <lacosh@gmail.com>
2021-10-24 00:40:37 +0300

    hw3

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -6,29 +6,29 @@ import com.badlogic.gdx.math.Vector2;
 import ru.gb.base.BaseScreen;
 import ru.gb.math.Rect;
 import ru.gb.sprite.Background;
+import ru.gb.sprite.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
     private Texture img;
-    private Vector2 pos;
-
     private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
-
         img = new Texture("badlogic.jpg");
-        pos = new Vector2();
+        logo = new Logo(img);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
@@ -36,7 +36,8 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
         batch.begin();
         background.draw(batch);
-        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
+        logo.update(delta);
+        logo.draw(batch);
         batch.end();
     }
 
@@ -49,7 +50,7 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        pos.set(touch);
+        logo.touchDown(touch, pointer, button);
         return super.touchDown(touch, pointer, button);
     }
 

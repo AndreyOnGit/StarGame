114133a0a26981540efbd04119d6fd2bd7532253
AndreyOnGit <lacosh@gmail.com>
2021-11-05 22:34:23 +0300

    after lesson 7

--- core/src/ru/gb/base/Ship.java
+++ core/src/ru/gb/base/Ship.java
@@ -6,10 +6,16 @@ import com.badlogic.gdx.math.Vector2;
 
 import ru.gb.math.Rect;
 import ru.gb.pool.BulletPool;
+import ru.gb.pool.ExplosionPool;
 import ru.gb.sprite.Bullet;
+import ru.gb.sprite.Explosion;
+
 
 public class Ship extends Sprite {
 
+    private static final float DAMAGE_ANIMATE_INTERVAL = 0.1f;
+
+    protected ExplosionPool explosionPool;
     protected BulletPool bulletPool;
     protected Sound bulletSound;
     protected TextureRegion bulletRegion;
@@ -26,6 +32,8 @@ public class Ship extends Sprite {
     protected float reloadInterval;
     protected Rect worldBounds;
 
+    private float damageAnimateTimer = DAMAGE_ANIMATE_INTERVAL;
+
     public Ship() {
     }
 
@@ -36,18 +44,47 @@ public class Ship extends Sprite {
     @Override
     public void update(float delta) {
         pos.mulAdd(v, delta);
-        bulletPos.set(pos);
         reloadTimer += delta;
         if (reloadTimer >= reloadInterval) {
             reloadTimer = 0f;
+            bulletPos.set(pos);
             shoot();
+        }
+        damageAnimateTimer += delta;
+        if (damageAnimateTimer >= DAMAGE_ANIMATE_INTERVAL) {
+            frame = 0;
         }
     }
 
-    protected void shoot() {
+    public void damage(int hp) {
+        this.hp -= hp;
+        if (this.hp <= 0) {
+            this.hp = 0;
+            destroy();
+        }
+        damageAnimateTimer = 0f;
+        frame = 1;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        boom();
+    }
+
+    private void shoot() {
         Bullet bullet = bulletPool.obtain();
         bullet.set(this, bulletRegion, bulletPos, bulletV, worldBounds, bulletHeight, damage);
         bulletSound.play();
+    }
+
+    private void boom() {
+        Explosion explosion = explosionPool.obtain();
+        explosion.set(this.pos, getHeight());
     }
 
 }
--- core/src/ru/gb/sprite/EnemyShip.java
+++ core/src/ru/gb/sprite/EnemyShip.java
@@ -7,23 +7,29 @@ import com.badlogic.gdx.math.Vector2;
 import ru.gb.base.Ship;
 import ru.gb.math.Rect;
 import ru.gb.pool.BulletPool;
+import ru.gb.pool.ExplosionPool;
 
 public class EnemyShip extends Ship {
 
-    public EnemyShip(BulletPool bulletPool, Rect worldBounds, Sound bulletSound) {
+    public EnemyShip(BulletPool bulletPool, ExplosionPool explosionPool, Rect worldBounds, Sound bulletSound) {
         this.bulletPool = bulletPool;
+        this.explosionPool = explosionPool;
         this.worldBounds = worldBounds;
         this.bulletSound = bulletSound;
         this.bulletV = new Vector2();
         this.bulletPos = new Vector2();
         this.v = new Vector2();
         this.v0 = new Vector2();
-        this.reloadTimer = 10f;
     }
 
     @Override
     public void update(float delta) {
         super.update(delta);
+        if (getTop() < worldBounds.getTop()) {
+            v.set(v0);
+        } else {
+            reloadTimer = reloadInterval * 0.8f;
+        }
         if (getBottom() < worldBounds.getBottom()) {
             destroy();
         }
@@ -41,7 +47,7 @@ public class EnemyShip extends Ship {
             float height
     ) {
         this.regions = regions;
-        this.v.set(v);
+        this.v0.set(v);
         this.bulletRegion = bulletRegion;
         this.bulletHeight = bulletHeight;
         this.bulletV.set(bulletV);
@@ -49,5 +55,13 @@ public class EnemyShip extends Ship {
         this.hp = hp;
         this.reloadInterval = reloadInterval;
         setHeightProportion(height);
+        this.v.set(0, -0.5f);
+    }
+
+    public boolean isBulletCollision(Bullet bullet) {
+        return !(bullet.getRight() < getLeft()
+                || bullet.getLeft() > getRight()
+                || bullet.getBottom() > getTop()
+                || bullet.getTop() < pos.y);
     }
 }
